e433f28b93acea3f83753da996e98bba5967f513
root <root@192.168.0.3>
2016-05-05 12:41:09 +0600

    api doc

--- src/AppBundle/Controller/PaymentController.php
+++ src/AppBundle/Controller/PaymentController.php
@@ -8,6 +8,7 @@ use AppBundle\Service\BdmobileService;
 
 class PaymentController extends FOSRestController
 {
+
     public function indexAction()
     {        
         $view = View::create()
@@ -16,6 +17,29 @@ class PaymentController extends FOSRestController
     }
     
     public function mobileAction(){
+        $mobileService = $this->get('app.bd_mobile');
+        $key = $mobileService->subscribe();
+        $view = View::create()
+            ->setData(array('thread' => $key));
+        return $this->handleView($view);
+    }
+    
+    
+    /**
+     * This is the documentation description of your method, it will appear
+     * on a specific pane. It will read all the text until the first
+     * annotation.
+     *
+     * @ApiDoc(
+     *  resource=true,
+     *  description="This is a description of your API method",
+     *  filters={
+     *      {"name"="a-filter", "dataType"="integer"},
+     *      {"name"="another-filter", "dataType"="string", "pattern"="(foo|bar) ASC|DESC"}
+     *  }
+     * )
+     */
+    public function statusAction(){
         $mobileService = $this->get('app.bd_mobile');
         $key = $mobileService->subscribe();
         $view = View::create()
